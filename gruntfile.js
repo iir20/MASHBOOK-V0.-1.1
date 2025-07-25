9378ccda4c59246f84ce939bf67d9ec0eece77a8
iir20 <ismailratul340@gmail.com>
2025-07-26 04:35:17 +0600

    Create gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,7 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    // your grunt config here
+  });
+  grunt.loadNpmTasks('grunt-contrib-xyz'); // load required plugins
+  grunt.registerTask('default', ['xyz']);
+}
